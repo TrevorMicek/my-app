2832bd85139e913a7e202c5305fb38e63d4939e6
Trevor Micek <miceking@comcast.net>
2020-05-16 18:34:01 -0600

    bottom nav location

--- src/trevormicek.github.io/SecondPage/Footer/Bottom.js
+++ src/trevormicek.github.io/SecondPage/Footer/Bottom.js
@@ -10,26 +10,34 @@ import Wrapper from '../../Styles/Wrapper';
 //Bottom Nav 
 const Container = styled(Wrapper)`
     position: absolute;
-    left: 50%;
+    text-align: center;
     bottom: 3vh;
+    width: 92vw;
+    
     display: inline-block;
-
+    @media(max-width: 768px) {
+        min-width: 50%;
+    }
     @media(min-width: 916px) {
    
-   font-size: 1.45em;
-   
-}
-    
+        font-size: 1.45em;
+        
+    }
 `;
 const SectionText = styled(Text)`
     display: inline-block;
+    position: relative;
     font-size: .5em;
     line-height: 1.5em;
     z-index: 5;
-    right: 50%;
-    margin-right: 1.5vw;
-    text-decoration: underline;
     
+    margin-right: 1.5vw;
+
+    text-decoration: underline;
+    @media(max-width: 768px) {
+        
+        font-size: .65em;
+    }
     
 `;
 const Bottom = () => (
