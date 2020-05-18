8726cb2ef0d58684ca2a57ae8e7e42efed1697d6
Trevor Micek <miceking@comcast.net>
2020-05-18 13:45:27 -0600

    readme update + image height

--- src/trevormicek.github.io/FrontPage/MainSection/SecondarySection.js
+++ src/trevormicek.github.io/FrontPage/MainSection/SecondarySection.js
@@ -13,7 +13,7 @@ import Text from '../../Styles/SectionText';
 const Container = styled(MainSectionItem)`
     background: url(${ForestImg}) no-repeat top center;
     background-size: 100% 100%;
-    height: 35vh;
+    height: 37.5vh;
 
     @media(max-width: 768px) {
         font-size: 1.25em;
@@ -34,7 +34,7 @@ const Container = styled(MainSectionItem)`
     }
 `;
 const OverlayStyle = styled(Overlay)`
-    background: rgba(00, 00, 00, .05);
+    background: rgba(00, 00, 00, .2);
     
     
 `;
